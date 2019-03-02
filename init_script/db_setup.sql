46d0c67b8ef07980b0bfd48df13ccd28cbacd149
thomaswhocares <thomas.steffens@outlook.com>
2019-03-02 18:45:38 +0100

    auflösungs tabellen

--- init_script/db_setup.sql
+++ init_script/db_setup.sql
@@ -5,6 +5,8 @@ USE gut_app;
 /*------------- Haupt- Tabellen ---------------*/
 CREATE TABLE tbl_abschlüsse(
     pk_abschluss_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    abschluss_name VARCHAR(100) NOT NULL,
+    
 
     PRIMARY KEY(pk_abschluss_id)
 )ENGINE=InnoDB;
@@ -35,5 +37,17 @@ CREATE TABLE tbl_lehrer_bildungsgänge(
         REFERENCES tbl_bildungsgänge(pk_bildungsgang_id),
     FOREIGN KEy (pk_fk_lehrer_id)
         REFERENCES tbl_lehrer(pk_lehrer_id)
-        
+)ENGINE=InnoDB;
+
+CREATE TABLE tbl_bildungsgänge_abschlüsse(
+    pk_fk_bildungsgang_id MEDIUMINT NOT NULL,
+    pk_fk_abschluss_id MEDIUMINT NOT NULL,
+
+    PRIMARY KEY(pk_fk_bildungsgang_id,pk_fk_abschluss_id),
+
+    FOREIGN KEY (pk_fk_bildungsgang_id) 
+        REFERENCES tbl_bildungsgänge(pk_bildungsgang_id),
+    FOREIGN KEy (pk_fk_abschluss_id)
+        REFERENCES tbl_abschlüsse(pk_abschluss_id)
+
 )ENGINE=InnoDB;
