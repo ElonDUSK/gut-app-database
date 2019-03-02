c00c47f79fe954cdca7a00ad60e6a9ef177d280d
thomaswhocares <thomas.steffens@outlook.com>
2019-03-02 19:03:00 +0100

    Tabellen von der webseite geklaut

--- init_script/db_setup.sql
+++ init_script/db_setup.sql
@@ -3,23 +3,59 @@ CREATE DATABASE gut_app;
 USE gut_app;
 
 /*------------- Haupt- Tabellen ---------------*/
+
+/*z.b. Technik,Gestaltung...  https://www.bkgut.de/bildungsangebote/  */
+CREATE TABLE tbl_kategorie(
+    pk_kategorie_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    kategorie_name VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(pk_kategorie_id)
+)ENGINE=InnoDB;
+
+/*z.b. Berufsvorbereitung,Gesundheitstechnik...  https://www.bkgut.de/bildungsangebote/  */
+CREATE TABLE tbl_bereich(
+    pk_bereich_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    bereich_name VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(pk_bereich_id)
+)ENGINE=InnoDB;
+
+/*z.b. Berufsschule,Höhere Berufsfachschule,Ausbildungsvorbereitung...  https://www.bkgut.de/bildungsangebote/  */
+CREATE TABLE tbl_schulform(
+    pk_schulform_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    schulform_name VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(pk_schulform_id)
+)ENGINE=InnoDB;
+
+CREATE TABLE tbl_kategorie(
+    pk_kategorie_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+    kategorie_name VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(pk_kategorie_id)
+)ENGINE=InnoDB;
+
+
+
 CREATE TABLE tbl_abschlüsse(
     pk_abschluss_id MEDIUMINT NOT NULL AUTO_INCREMENT,
     abschluss_name VARCHAR(100) NOT NULL,
-    
 
     PRIMARY KEY(pk_abschluss_id)
 )ENGINE=InnoDB;
 
 CREATE TABLE tbl_bildungsgänge(
     pk_bildungsgang_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-
+    bildungsgang_dauer_jahre INT NOT NULL,
     PRIMARY KEY(pk_bildungsgang_id)
 )ENGINE=InnoDB;
 
 CREATE TABLE tbl_lehrer(
     pk_lehrer_id MEDIUMINT NOT NULL AUTO_INCREMENT,
-
+    lehrer_nachname VARCHAR(100) NOT NULL,
+    lehrer_vorname VARCHAR(100) NOT NULL,
+    lehrer_telefon_nummer VARCHAR(100) NOT NULL,
+    lehrer_email_adresse VARCHAR(300) NOT NULL,
     PRIMARY KEY(pk_lehrer_id)
 )ENGINE=InnoDB;
 
